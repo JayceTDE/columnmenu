b0fc556f591874613da3f4ecb662e3ea691fdc90
Jayce Pulsipher <jaycemp@gmail.com>
2013-09-28 12:23:22 -0600

    Modified into a tree menu

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ function Menu() {
     self.items = {};
     self.el = document.createElement('ul');
     self.el.className = 'menu';
-    mouseInOut.bind(self.el, 'mouseenter', self.deselect.bind(self));
     mouseInOut.bind(self.el, 'mouseleave', function () {
         clearTimeout(self._hoverTimer);
     });
@@ -59,87 +58,6 @@ function Menu() {
 Menu.prototype = new Emitter;
 
 /**
- * Deselect selected menu items.
- *
- * @api private
- */
-
-Menu.prototype.deselect = function () {
-    var selected = query('.selected', this.el);
-    selected && classes(selected).remove('selected');
-};
-
-/**
- * Bind keyboard events.
- *
- * @api private
- */
-
-//Menu.prototype.bindKeyboardEvents = function(){
-//  o(document).bind('keydown.menu', this.onkeydown.bind(this));
-//  return this;
-//};
-
-/**
- * Unbind keyboard events.
- *
- * @api private
- */
-
-//Menu.prototype.unbindKeyboardEvents = function(){
-//  o(document).unbind('keydown.menu');
-//  return this;
-//};
-
-/**
- * Handle keydown events.
- *
- * @api private
- */
-
-/*
-Menu.prototype.onkeydown = function(e){
-  switch (e.keyCode) {
-    // esc
-    case 27:
-      this.hide();
-      break;
-    // up
-    case 38:
-      e.preventDefault();
-      this.move('prev');
-      break;
-    // down
-    case 40:
-      e.preventDefault();
-      this.move('next');
-      break;
-  }
-};
-*/
-
-/**
- * Focus on the next menu item in `direction`.
- *
- * @param {String} direction "prev" or "next"
- * @api public
- */
-
-/*Menu.prototype.move = function(direction){
-  var prev = this.el.find('.selected').eq(0);
-
-  var next = prev.length
-    ? prev[direction]()
-    : this.el.find('li:first-child');
-
-  if (next.length) {
-    prev.removeClass('selected');
-    next.addClass('selected');
-    next.find('a').focus();
-  }
-};*/
-
-/**
  * Add menu item with the given `text` and optional callback `fn`.
  *
  * When the item is clicked `fn()` will be invoked
@@ -153,115 +71,72 @@ Menu.prototype.onkeydown = function(e){
  * @api public
  */
 
-Menu.prototype.add = function (text, value) {
+Menu.prototype.data = function (arr) {
+    for (var i = 0, l = arr.length; i < l; i += 1) {
+        this.add(arr[i]);
+    }
+};
+
+Menu.prototype.add = function (obj) {
     
     var self = this
-      , slug
+      , el = document.createElement('li')
+      , sub
+      , o
     ;
     
-    if (typeof text === 'object') {
-        for (var prop in text) {
-            text.hasOwnProperty(prop) && self.add(prop, text[prop]);
-        }
-        return;
-    }
+    el.innerHTML = '<a href="#">' + obj.name + '</a>';
+    el.className = 'menu-item';
     
-    slug = createSlug(text);
+    self.el.appendChild(el);
     
-    if (typeof value === 'object') {
-        // Create submenu
-        var sub = new Menu()
-        sub.value = value.value;
-        delete value.value;
-        sub.add(value);
+    if (obj.children) {
+        sub = new Menu();
+        sub.data(obj.children);
         sub._parent = self;
         sub.hide();
         document.body.appendChild(sub.el);
-        value = sub;
+        el.className += ' menu-parent';
     }
     
-    var el = document.createElement('li');
-    
-    el.innerHTML = '<a href="#">' + text + '</a>';
-    el.className = 'menu-item-' + slug;
-    el.className += value instanceof Menu ? ' submenu' : '';
-    this.el.appendChild(el);
     events.bind(el, 'click', function (e) {
-        
         prevent(e);
         e.stopPropagation();
         
         clearTimeout(self._hoverTimer);
-        
         self._current && self._current.hide();
         
-        if (value instanceof Menu && typeof value.value === 'undefined') {
-            var o = offset(el);
-            value.moveTo(o.left + el.offsetWidth, o.top);
-            value.show();
-            self._current = value;
-        } else {
-            self.select(self.items[slug]);
-        }
+        self._current = null;
         
+        if (sub) {
+            if (obj.value) return self.select(obj);
+            o = offset(el);
+            sub.moveTo(o.left + el.offsetWidth, o.top - 1);
+            
+            sub.show();
+            self._current = sub;
+        } else {
+            self.select(obj);
+        }
     });
     
     mouseInOut.bind(el, 'mouseenter', function (e) {
-        
         clearTimeout(self._hoverTimer);
-        
         self._hoverTimer = setTimeout(function () {
-            
             self._current && self._current.hide();
+            self._current = null;
             
-            if (value instanceof Menu) {
-                var o = offset(el);
-                value.moveTo(o.left + el.offsetWidth, o.top);
-                value.show();
-                self._current = value;
+            if (sub) {
+                o = offset(el);
+                sub.moveTo(o.left + el.offsetWidth, o.top - 1);
+                
+                sub.show();
+                self._current = sub;
             }
-            
-        }, 500);
-        
+        })
     });
-
-    self.items[slug] = { slug: slug, text: text, el: el, value: value };
+    
     return self;
-};
-
-/**
- * Remove menu item with the given `slug`.
- *
- * @param {String} slug
- * @return {Menu}
- * @api public
- */
-
-/*Menu.prototype.remove = function (slug) {
-    slug = createSlug(slug);
-    var item = this.items[slug];
-    if (!item) throw new Error('no menu item named "' + slug + '"');
-    this.emit('remove', slug);
-    if (item._subs) {
-        for (var i = 0, l = item._subs.length; i < l; i += 1) {
-            item._subs[i].el.parentNode.removeChild(item._subs[i].el);
-        }
-    }
-    item.el.parentNode.removeChild(item.el);
-    delete this.items[slug];
-    return this;
-};*/
-
-/**
- * Check if this menu has an item with the given `slug`.
- *
- * @param {String} slug
- * @return {Boolean}
- * @api public
- */
-
-Menu.prototype.has = function (slug) {
-    return !!this.items[createSlug(slug)];
 };
 
 /**
@@ -310,18 +185,3 @@ Menu.prototype.select = function (item) {
     this.hide();
     return this._parent ? this._parent.select(item) : this.emit('select', item);
 };
-
-/**
- * Generate a slug from `str`.
- *
- * @param {String} str
- * @return {String}
- * @api private
- */
-
-function createSlug(str) {
-    return String(str)
-        .toLowerCase()
-        .replace(/ +/g, '-')
-        .replace(/[^a-z0-9-]/g, '');
-}
